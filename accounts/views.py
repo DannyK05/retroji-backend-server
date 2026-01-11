d6d86a17bcdd99aa2f4bbdd9819033c42ce5738d
Kolade <olubanjok@gmail.com>
2026-01-11 19:26:55 +0100

    chore(auth): updated auth endpoints message and enabled debug

--- accounts/views.py
+++ accounts/views.py
@@ -13,17 +13,17 @@ def register(request):
     password = request.data.get("password")
 
     if not username or not password:
-        return Response({"error": "Username and password are required"},
+        return Response({"message": "Username and password are required", "status":status.HTTP_400_BAD_REQUEST},
                         status=status.HTTP_400_BAD_REQUEST)
 
     if User.objects.filter(username=username).exists():
-        return Response({"error": "Username already taken"},
+        return Response({"message": "Username already taken", "status":status.HTTP_400_BAD_REQUEST},
                         status=status.HTTP_400_BAD_REQUEST)
 
     user = User.objects.create_user(username=username, email=email, password=password)
     serialized_user = UserSerializer(user)
 
-    return Response({"message": "User Created Successfully", "user": serialized_user.data},
+    return Response({"message": "Account created", "data":{"user": serialized_user.data,}, "status": status.HTTP_201_CREATED},
                     status=status.HTTP_201_CREATED)
 
 
@@ -37,14 +37,14 @@ def login(request):
     if user is not None:
         django_login(request, user)
         serialized_user = UserSerializer(user)
-        return Response({"message": "Login Successfully", "user": serialized_user.data},
+        return Response({"message": "You are logged in", "data":{"user": serialized_user.data,}, "status": status.HTTP_200_OK},
                         status=status.HTTP_200_OK)
-
-    return Response({"message": "Invalid username or password"},
-                    status=status.HTTP_404_NOT_FOUND)
+    
+    return Response({"message": "Invalid username or password", "status":status.HTTP_401_NOT_FOUND},
+                    status=status.HTTP_401_NOT_FOUND)
 
 
 @api_view(['POST'])
 def logout(request):
     django_logout(request)
-    return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
+    return Response({"message": "You are logged out",  "status": status.HTTP_200_OK}, status=status.HTTP_200_OK)
