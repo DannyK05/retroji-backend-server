0c3df8deca35e2bb15119a6772187a7aa7830a95
Kolade <olubanjok@gmail.com>
2026-03-30 10:31:06 +0100

    chore(snapz): update url for snapz views

--- snapz/urls.py
+++ snapz/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     # Snapz
-    path('snapz/', views.get_all_snapz, name="get_all_snapz"),
-    path('snapz/post/', views.post_snapz, name="post_snapz"),
-    path('snapz/<uuid:snapz_id>/', views.get_snapz_by_id, name="get_snapz_by_id"),
+    path('', views.get_all_snapz, name="get_all_snapz"),
+    path('post/', views.post_snapz, name="post_snapz"),
+    path('<uuid:snapz_id>/', views.get_snapz_by_id, name="get_snapz_by_id"),
 
     # Comments
-    path('snapz/<uuid:snapz_id>/comments/', views.get_all_comments_by_snapz_id, name="get_all_comments_by_snapz_id"),
-    path('snapz/comment/post/', views.post_comment, name="post_comment"),
+    path('<uuid:snapz_id>/comments/', views.get_all_comments_by_snapz_id, name="get_all_comments_by_snapz_id"),
+    path('comment/post/', views.post_comment, name="post_comment"),
 
     # Likes
-    path('snapz/like/', views.like, name="like_post"),
+    path('like/', views.like, name="like_post"),
 ]
