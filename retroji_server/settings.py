e76928afb1a87d3c36d4a34bec66d15179524112
Kolade <olubanjok@gmail.com>
2026-04-01 19:38:58 +0100

    fix: update secret key

--- retroji_server/settings.py
+++ retroji_server/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$r5#z63+%no!8qi8=-sldfsd@%9ji6)f8$^e!_nem#%13kss1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
