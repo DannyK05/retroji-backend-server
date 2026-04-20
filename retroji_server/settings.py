dc3acdd8cd776e9b84b6fa8fad3fd01e5fddebc2
Kolade <olubanjok@gmail.com>
2026-04-20 17:02:28 +0100

    chore: make migrations for snapz; setup db condition for prod and dev

--- retroji_server/settings.py
+++ retroji_server/settings.py
@@ -115,11 +115,19 @@ SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(days=1), "REFRESH_TOKEN_LIFETIM
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+if config('DEBUG', cast=bool):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
 
 
 # Password validation
